3d477cfd2e4500ced4d27c78d28247ff8bd28827
UsaidAhmed31 <usaid.ahmedmay@gmail.com>
2024-09-02 01:17:52 +0500

    Added Devski Services

---
+++ src/Components/DevskiServices.js
@@ -0,0 +1,132 @@
+import React from "react";
+import webdevIcon from '../Images/webdevIcon.png';
+import MobileDevIcon from '../Images/MobileDevIcon.png';
+import DigitalIcon from '../Images/DigitalIcon.png';
+import CloudIcon from '../Images/CloudIcon.png';
+import softDevIcon from '../Images/softDevIcon.png';
+import brandingIcon from '../Images/brandingIcon.png';
+import Project1 from '../Images/Project1.png';
+import Project2 from '../Images/Project2.png';
+import CardImage from '../Images/CardImage.png';
+import DevskiServicesCss from '../Css/DevskiServices.module.css';
+
+
+function DevskiServices(){
+    return(
+        <>
+        <div className={DevskiServicesCss.main}>
+        <div className={DevskiServicesCss.Topimage}>
+           <h1>Our <span> Services</span></h1>
+            </div>
+        <div className={DevskiServicesCss.ArrayofServices}>
+            <h2>We offer a wide array of services</h2>
+            
+            <div className={DevskiServicesCss.MainContainer}>
+            <div className={DevskiServicesCss.ContainerBlock1}>
+            <div className={DevskiServicesCss.Container}>
+                <img src = {webdevIcon}className={DevskiServicesCss.webdevIcon} alt = "webdevIcon"/>
+                 <h1>Web Development</h1>
+                 <p>Non tristique et nec nibh nisl aliquet et amet posuere. Vel nunc aliquam magna lacinia nulla risus. </p>
+                 <button className={DevskiServicesCss.LearnMore}>Learn More</button>
+            </div>
+            <div className={DevskiServicesCss.Container}>
+            <img src = {MobileDevIcon}className={DevskiServicesCss.MobileDevIcon} alt = "MobileDevIcon"/>
+                 <h1>Mobile Development</h1>
+                 <p>Non tristique et nec nibh nisl aliquet et amet posuere. Vel nunc aliquam magna lacinia nulla risus. </p>
+                 <button className={DevskiServicesCss.LearnMore}>Learn More</button>
+            </div>
+            <div className={DevskiServicesCss.Container}>
+            <img src = {softDevIcon}className={DevskiServicesCss.softDevIcon} alt = "softDevIcon"/>
+                 <h1>Software Development</h1>
+                 <p>Non tristique et nec nibh nisl aliquet et amet posuere. Vel nunc aliquam magna lacinia nulla risus. </p>
+                 <button className={DevskiServicesCss.LearnMore}>Learn More</button>
+            </div>
+            </div>
+
+            <div className={DevskiServicesCss.ContainerBlock2}>
+            <div className={DevskiServicesCss.Container}>
+            <img src = {brandingIcon}className={DevskiServicesCss.brandingIcon} alt = "brandingIcon"/>
+                 <h1>Branding</h1>
+                 <p>Non tristique et nec nibh nisl aliquet et amet posuere. Vel nunc aliquam magna lacinia nulla risus. </p>
+                 <button className={DevskiServicesCss.LearnMore}>Learn More</button>
+            </div>
+            <div className={DevskiServicesCss.Container}>
+            <img src = {DigitalIcon}className={DevskiServicesCss.DigitalIcon} alt = "DigitalIcon"/>
+                 <h1>Digital Marketing</h1>
+                 <p>Non tristique et nec nibh nisl aliquet et amet posuere. Vel nunc aliquam magna lacinia nulla risus. </p>
+                 <button className={DevskiServicesCss.LearnMore}>Learn More</button>
+            </div>
+            <div className={DevskiServicesCss.Container}>
+            <img src = {CloudIcon}className={DevskiServicesCss.CloudIcon} alt = "CloudIcon"/>
+                 <h1>Cloud Services</h1>
+                 <p>Non tristique et nec nibh nisl aliquet et amet posuere. Vel nunc aliquam magna lacinia nulla risus. </p>
+                 <button className={DevskiServicesCss.LearnMore}>Learn More</button>
+            </div>
+            </div>
+
+
+            </div>
+        </div>
+
+        <div className={DevskiServicesCss.BlackBg}>
+            <h1>Our Projects</h1>
+            <div className={DevskiServicesCss.ProjectButtons}>
+            <button className={DevskiServicesCss.AllButton}> All </button>
+            <button className={DevskiServicesCss.WebButton}> Web </button>
+            <button className={DevskiServicesCss.AppButton}> App </button>
+            <button className={DevskiServicesCss.UIUXButton}> UI/UX </button>
+            </div>
+
+            <div className={DevskiServicesCss.AllProjects}>
+            <div className={DevskiServicesCss.proj1}>
+            <img src = {Project1}className={DevskiServicesCss.Project1} alt = "Project1"/>
+            <h1>Helping Hands</h1>
+            <p>Website</p>
+            </div>
+            <div className={DevskiServicesCss.proj2}>
+            <img src = {Project2}className={DevskiServicesCss.Project2} alt = "Project2"/>
+            <h1>Nom Nom Nerds</h1>
+            <p>Web Design</p>
+            </div>
+            </div>
+
+            <div className={DevskiServicesCss.AllProjects2}>
+            <div className={DevskiServicesCss.proj3}>
+            <img src = {Project1}className={DevskiServicesCss.Project1} alt = "Project1"/>
+            <h1>Cafe 90s</h1>
+            <p>Website</p>
+            </div>
+            <div className={DevskiServicesCss.proj4}>
+            <img src = {Project2}className={DevskiServicesCss.Project2} alt = "Project2"/>
+            <h1>TechTree</h1>
+            <p>Web Design</p>
+            </div>
+            </div>
+        </div>
+        
+        <div className={DevskiServicesCss.MainCard}>
+        <div className={DevskiServicesCss.Card}>
+            <div className={DevskiServicesCss.CardText}>
+            <h1>Help To Build Your Dream Project</h1>
+            <p>A digital agency is a business you hire to outsource your digital marketing efforts, instead of handling in-house.</p>
+            <button className={DevskiServicesCss.CardButton}>Discuss Your Project <span> </span> <svg xmlns="http://www.w3.org/2000/svg" width="10" height="12" viewBox="0 0 10 12" fill="none">
+<path d="M10 6L0 11.7735V0.226501L10 6Z" fill="#111204"/>
+</svg></button>
+            </div>
+            <img src = {CardImage} className={DevskiServicesCss.CardImage} alt = "CardImage"/>
+        </div>
+        
+        </div>
+
+        <div className={DevskiServicesCss.FooterMain}>
+            <h1>LETS WORK TOGETHER</h1>
+        </div>
+
+        </div>    
+
+        </>
+
+    )
+}
+
+export default DevskiServices
